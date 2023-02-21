84fa174484bcded0364199acde6a770a9ef48636
Isaac Mwaura <isaacmwauran@gmail.com>
2023-02-21 12:34:48 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#include <main.h>
+
+int _putchar(char c); 
+1-alphabet.c 
+2-print_alphabet(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void  jack_bauer(void);
+void times_tablec(void);
+int add(int, int);
+void print_to_98(int n);
+void print_time_table(int n);
