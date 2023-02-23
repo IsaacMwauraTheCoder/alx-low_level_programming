fc7c6936c4315d51c4a87d8901920339ba359602
Isaac Mwaura <isaacmwauran@gmail.com>
2023-02-23 07:34:03 -0800

    Nested loops, more functions

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - function that checks a digit 0 - 9
+ * @c: input
+ * Return: 1 if c is digit an d 0 otherweise
+ */
+int _isdigit(int c)
+	{
+		((c >= 48) && (c <= 57))
+
+	return (1);
+		else
+			return (0);
+	}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers  - print numbers 0 - 9
+ * Return: 0
+ */
+
+int print_numbers(void)
+	{
+		int i;
+	for (i = 0; i <= 9; i++)
+		{
+		_putchar((n % 10) + '0');
+	}
+	_putchar('\n');
+	}
