ac6b9efa9b293ded002a9c981fb3ed9c6e250abd
Isaac Mwaura <isaacmwauran@gmail.com>
2023-02-23 08:09:35 -0800

    more functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers 0-9except 2 & 4
+ * Return: 0
+ */
+
+int print_most_numbers(void)
+	{
+
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+	if (i != 2 && n != 4)
+	}
+	putchar((i % 10) + '0');
+	}
+}
+_putchar('\n');
+}
