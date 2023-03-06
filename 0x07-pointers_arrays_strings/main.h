380439b07f7185292f2788eade57c9dcb8bb1fe4
Isaac Mwaura <isaacmwauran@gmail.com>
2023-03-06 11:24:32 -0800

    Correct syntax

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -6,4 +9,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-int _putchar(char c)
+int _putchar(char c);
+
+#endif
