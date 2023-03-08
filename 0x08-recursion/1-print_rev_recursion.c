89a1736d2dc06f4f0588dcacd35a22b913ba8b8f
Isaac Mwaura <isaacmwauran@gmail.com>
2023-03-08 13:22:45 -0800

    recusion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * void _print_rev_recursion - printing a string in reverse
+ * _print_rev_recursion - printing a string in reverse
+ * @s: pointer to first address
  * Return: string
  */
 
 void _print_rev_recursion(char *s)
-        {
-                if (*s)
-                {
-                _putchar(*s);
-                _print_rev_recursion(s+1);
-        }
-                _putchar('\n');
-                }
-~                     
+	{
+		if (*s)
+		{
+			_putchar(*s);
+			_print_rev_recursion(s + 1);
+		}
+		_putchar('\n');
+	}
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * int _strlen_recursion - counts the length of string
+ * _strlen_recursion - counts the length of string
+ * @s: pointer to string
  * Return: length
  */
 
 int _strlen_recursion(char *s)
 	{
-	
+
 	int l = 0;
 
 	if (*s)
 		{
-		l++
-		l = l + _strlen_recursion(s+1);
+		l++;
+
+		l = l + (_strlen_recursion(s + 1));
 		}
-	return(l);	
+	return (l);
 	}
