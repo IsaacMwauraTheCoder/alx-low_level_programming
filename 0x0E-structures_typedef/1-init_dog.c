a50b2beb76cdc848bf1fbf7b45dd4c70a39b589c
Isaac Mwaura <isaacmwauran@gmail.com>
2023-03-20 13:18:33 -0700

    structures

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * void init_dog - initializing a variable
+ *  @d: pointer to struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
+ * Return: 0
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+
+{
+		if (d == NULL)
+			d = malloc(sizeof(struct dog));
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * void print_dog - function for printing
+ * @d: return nil
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+			return;
+
+
+		if (d->name == NULL)
+			d->name = "(nil)";
+		if (d->owner == NULL)
+			d->owner = "(nil)";
+		
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
